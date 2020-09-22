a106e5e825d1899c29b01683bd9f456ea8082e30
zanxingyu <zanxingyu@kkworld.com>
2020-09-22 17:54:45 +0800

    update

--- ddd/code_snippet/compilation.kt
+++ ddd/code_snippet/compilation.kt
@@ -1,7 +1,16 @@
 fun create(cmd: CreateCommand) {
     val permissions = getUserPermissionsFromExternalService(cmd.uid)
     if (!permissions.canPublishCompilation) throw Exception("无权限")
-    if (!valid(cmd)) throw Exception("参数错误")
+    validate(cmd)
     val censorResult = checkContentByCensorship(cmd)
-    if (!censorResult.pass) throw Exception("")
+    if (!censorResult.pass) throw Exception("未通过初审")
+    val status = if (censorResult.setPublic) Status.PUBLIC else Status.PRIVATE
+    
+    dao.insertCompilation(cmd.uid, cmd.title, cmd.description, cmd.cover, status)
+
+}
+
+fun validate(cmd: CreateCommand) {
+    if (cmd.title.length >30) throw Exception("标题过长")
+    if (cmd.description.length >80) throw Exception("描述过长")
 }
